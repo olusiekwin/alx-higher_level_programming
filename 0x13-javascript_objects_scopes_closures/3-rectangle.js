739a1dddc7a952a3d87776dac4a79999ff0ed1c3
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:14:11 +0300

    feat: Add Rectangle class with print() method
    - Define a Rectangle class with a constructor that initializes width and height attributes
    - Handle cases where width or height is <= 0, creating an empty object
    - Add an instance method print() to print the rectangle using the character X
    - Update main file (3-main.js) to demonstrate the print() method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,20 @@
-// 3-rectangle.js
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
