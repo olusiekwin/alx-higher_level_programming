f2504c7989206b1806dcd90ace2ba83bc15861a0
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:44:52 +0300

    feat: Implement script to concatenate two files
    - Create 102-concat.js script that takes two file paths as input and concatenates their contents to a third file
    - Print a success message if the operation is successful, or an error message if any issues occur

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,25 @@
-// 102-concat.js
-const fileA = fs.readFileSync(process.argv[2], 'utf8');
-const fileB = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], fileA + fileB);
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js fileA fileB fileC');
+  process.exit(1);
+}
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+try {
+  const contentA = fs.readFileSync(fileA, 'utf-8');
+  const contentB = fs.readFileSync(fileB, 'utf-8');
+
+  const concatenatedContent = `${contentA}${contentB}`;
+
+  fs.writeFileSync(fileC, concatenatedContent);
+
+  console.log(`Files ${fileA} and ${fileB} have been concatenated to ${fileC}`);
+} catch (err) {
+  console.error('Error:', err.message);
+  process.exit(1);
+}
