7421e78d10c97f28f5ef386b2de63566d4f88774
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:21:14 +0300

    feat: Implement Square class that inherits from Rectangle
    - Create Square class in 5-square.js
    - Square class extends Rectangle class
    - Square constructor calls Rectangle constructor using super(size, size)
    - Update main file (5-main.js) to demonstrate Square class

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,11 @@
-// 5-square.js
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    // Call the constructor of the parent class (Rectangle) using super()
+    super(size, size);
+  }
+}
+
+module.exports = Square;
