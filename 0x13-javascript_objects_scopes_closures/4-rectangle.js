67bdac56ae24a3137ccaeacde2ff6873a772629f
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:19:29 +0300

    feat: Enhance Rectangle class with rotate() and double() methods
    - Define rotate() method to exchange width and height
    - Define double() method to multiply width and height by 2
    - Update main file (4-main.js) to demonstrate the new methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        // If w or h is equal to 0 or not a positive integer, create an empty object
-        return {};
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
     }
-  
-    print() {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  
-    rotate() {
-      // Exchange the width and height of the rectangle
-      [this.width, this.height] = [this.height, this.width];
-    }
-  
-    double() {
-      // Multiply the width and height of the rectangle by 2
-      this.width *= 2;
-      this.height *= 2;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  rotate () {
+    // Exchange the width and height of the rectangle
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    // Multiply the width and height of the rectangle by 2
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
