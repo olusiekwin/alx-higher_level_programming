7d9647c4f7745b1c99fc0432cb1cf12014f7ff87
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:08:34 +0300

    chore: Implement Rectangle class with input validation
    - Refactor Rectangle class to handle cases where width or height is <= 0
    - If either value is invalid, return an empty object
    - Add main file (2-main.js) for testing

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,14 @@
-// 2-rectangle.js
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
