83fb9f80fb9d6e31e34898c65592a1b05d277f2c
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:39:46 +0300

    feat: Implement script to compute a new array using map
    - Create 100-map.js script that imports list from 100-data.js
    - Use map function to compute a new array with each value equal to the value of the initial list multiplied by its index
    - Update 100-data.js to export an array named list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
-// 100-map.js
-const mappedList = list.map((value, index) => value * index);
-console.log(list);
-console.log(mappedList);
+#!/usr/bin/node
+const data = require('./100-data');
+
+const initialList = data.list;
+const newList = initialList.map((value, index) => value * index);
+
+console.log(initialList);
+console.log(newList);
