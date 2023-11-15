3db868ca2888ec6966664f57207ec6cf237b2410
olusiekwin <olusiekwin@gmail.com>
2023-11-15 18:23:50 +0300

    feat: Implement Square class with charPrint(c) method
    - Create Square class in 6-square.js, inheriting from the previous Square class
    - Add charPrint(c) method to print the rectangle using the character c (default to 'X' if undefined)
    - Update main file (6-main.js) to demonstrate the charPrint(c) method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
-// 6-square.js
+#!/usr/bin/node
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      // If c is undefined, use the character X
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
