719c9f91644342079ac62a4d12140194b32bf276
olusiekwin <olusiekwin@gmail.com>
2023-11-18 14:15:08 +0300

    Add SQLAlchemy script to list cities with their states using relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,4 +18,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state')
-    
