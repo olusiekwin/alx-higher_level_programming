d206e23d20ce18705b82c5c0072179bd64d4a38c
olusiekwin <olusiekwin@gmail.com>
2023-12-04 12:24:40 +0300

    Add 0-body_size.sh script

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script takes a URL, sends a request, and displays the size of the response body in bytes.
+
+# Send a request to the specified URL, follow redirects, and output only the size of the body in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+
